ba2d3c5a94d4216411b74b000b480bb346d9ba5e
chavananand522 <chavananand522@gmail.com>
2026-09-20 23:03:06 +0530

    Describe what you changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
+import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 
 const App = () => {
     return (
@@ -10,7 +13,11 @@ const App = () => {
             <Hero />
 
             <div className="container">
+                <Title subTitle='Our PROGRAM' title='What We Offer' />
                 <Programs />
+                <About />
+                <Title subTitle='Gallery' title='Campus Photos' />
+                <Campus />
             </div>
         </div>
     )
